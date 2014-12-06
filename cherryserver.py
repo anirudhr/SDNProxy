6e526c8b71c15c71ab4602c519656b8f77e7acfb
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-12-06 16:30:31 -0500

    Proxy work done, blacklist added

--- cherryserver.py
+++ cherryserver.py
@@ -4,40 +4,28 @@
 
 import cherryproxy
 import sys, os
-"""
-The following attributes can be read and MODIFIED:
-    self.req.headers: dictionary of HTTP headers, with lowercase names
-    self.req.method: HTTP method, e.g. 'GET', 'POST', etc
-    self.req.scheme: protocol from URL, e.g. 'http' or 'https'
-    self.req.netloc: IP address or hostname of server, with optional
-                     port, for example 'www.google.com' or '1.2.3.4:8000'
-    self.req.path: path in URL, for example '/folder/index.html'
-    self.req.query: query string, found after question mark in URL
 
-The following attributes can be READ only:
-    self.req.environ: dictionary of request attributes following WSGI
-                      format (PEP 333)
-    self.req.url: partial URL containing 'path?query'
-    self.req.full_url: full URL containing 'scheme:netloc/path?query'
-    self.req.length: length of request data in bytes, 0 if none
-    self.req.content_type: content-type, for example 'text/html'
-    self.req.charset: charset, for example 'UTF-8'
-    self.req.url_filename: filename extracted from URL path
-"""
+with open('domain_blacklist') as f:
+    blist = f.readlines()
+    f.close()
+blacklist = [x.rstrip() for x in blist]
+
 class SDNProxy(cherryproxy.CherryProxy):
     def filter_request_headers(self):
         #self.log.debug(header location)
-    if False:#self.req.full_url ##########Block check
-        msg = "Request blocked"
-        #self.log.warning(msg)
-        self.set_response_forbidden(reason=msg)
-    else:
-        pass #SDN stuff here
+        if self.req.netloc in blacklist:
+            msg = "Request to " + self.req.netloc + " blocked by blacklist policy"
+            self.req.log.warning(msg)
+            self.set_response_forbidden(reason=msg)
+        else:
+            msg = "Request to " + self.req.netloc + " passed."
+            self.req.log.info(msg)
+            #SDN stuff here
     
     def filter_response_headers(self):
         pass
     
     def filter_response(self):
         pass
-    
+
 cherryproxy.main(SDNProxy)
