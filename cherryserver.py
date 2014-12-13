f335baa38d36b4a5b88931b1b7fb519fcc98762d
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-12-13 15:31:51 -0500

    classed it up

--- cherryserver.py
+++ cherryserver.py
@@ -1,20 +1,34 @@
 #!/usr/bin/python
 #:indentSize=4:tabSize=4:noTabs=true:wrap=soft:
 #http://www.decalage.info/python/cherryproxy
+#http://osrg.github.io/ryu/
 
 import cherryproxy
-import sys, os
+import logging
+import struct
 
-with open('domain_blacklist') as f:
-    blist = f.readlines()
-    f.close()
-blacklist = [x.rstrip() for x in blist]
+from ryu.base import app_manager
+from ryu.controller import mac_to_port
+from ryu.controller import ofp_event
+from ryu.controller.handler import MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_0
+from ryu.lib.mac import haddr_to_bin
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
 
 class SDNProxy(cherryproxy.CherryProxy):
+    def getBlacklist(self, filename='domain_blacklist'):
+        with open(filename) as f:
+            blist = f.readlines()
+            f.close()
+        blacklist = [x.rstrip() for x in blist]
+        return blacklist
     def filter_request_headers(self):
         #self.log.debug(header location)
-        if self.req.netloc in blacklist:
-            msg = "Request to " + self.req.netloc + " blocked by blacklist policy"
+        self.blacklist = self.getBlacklist()
+        if self.req.netloc in self.blacklist:
+            msg = "Request to " + self.req.netloc + " blocked by self.blacklist policy"
             self.req.log.warning(msg)
             self.set_response_forbidden(reason=msg)
         else:
@@ -28,4 +42,5 @@ class SDNProxy(cherryproxy.CherryProxy):
     def filter_response(self):
         pass
 
+############main
 cherryproxy.main(SDNProxy)
