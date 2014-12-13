b29e9b33f89fdf36fc5e6f7d5346cc40b213df3f
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-12-13 15:44:35 -0500

    decided to integrate cherryproxy code into ryu code

--- cherryserver.py
+++ cherryserver.py
@@ -7,16 +7,6 @@ import cherryproxy
 import logging
 import struct
 
-from ryu.base import app_manager
-from ryu.controller import mac_to_port
-from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER
-from ryu.controller.handler import set_ev_cls
-from ryu.ofproto import ofproto_v1_0
-from ryu.lib.mac import haddr_to_bin
-from ryu.lib.packet import packet
-from ryu.lib.packet import ethernet
-
 class SDNProxy(cherryproxy.CherryProxy):
     def getBlacklist(self, filename='domain_blacklist'):
         with open(filename) as f:
